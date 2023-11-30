dfbec95e9db7a2942c6fa1accf74d58e21b77272
kdnakt <a.kid.1985@gmail.com>
2023-12-01 00:12:25 +0900

    we need panic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 // disable standard library, not to use libc etc.
 #![no_std]
 
+use core::panic::PanicInfo;
+
 fn main() {
     // println!("Hello, world!");
+}
+
+// ! is the "never" type
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
