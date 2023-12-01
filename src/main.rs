0c5988d55450c71935350966ba8d13864175719d
kdnakt <a.kid.1985@gmail.com>
2023-12-01 10:12:43 +0900

    overwrite the os entry point with our own function

--- src/main.rs
+++ src/main.rs
@@ -14,3 +14,9 @@ use core::panic::PanicInfo;
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
+// disable name mangling to ensure the name "_start"
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
