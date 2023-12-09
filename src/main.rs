bbce762f526f3c11307fbce206a1dc8c0e0afe43
kdnakt <a.kid.1985@gmail.com>
2023-12-09 14:20:57 +0900

    printing to the console with serial port

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+mod serial;
 
 // fn main() {
 //     // println!("Hello, world!");
@@ -69,7 +70,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
@@ -78,7 +79,7 @@ fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... ");
+    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
