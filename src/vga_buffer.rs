2204e3cef02b00d6bd4341b9a4c87d35d27e1c56
kdnakt <a.kid.1985@gmail.com>
2023-12-10 13:12:18 +0900

    test WRITER output

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -171,3 +171,13 @@ fn test_println_many() {
         println!("test_println_many output");
     }
 }
+
+#[test_case]
+fn tets_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
