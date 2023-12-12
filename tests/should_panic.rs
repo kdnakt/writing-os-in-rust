fee0b4d958fe3e15eb996250999cac9f1d24e94d
kdnakt <a.kid.1985@gmail.com>
2023-12-13 00:43:06 +0900

    test without harness

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -19,7 +19,6 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[test_case]
 fn should_fail() {
     serial_print!("should_panic::should_fail...\t");
     assert_eq!(0, 1);
@@ -27,16 +26,8 @@ fn should_fail() {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
     loop {}
-}
-
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
 }
