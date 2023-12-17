7737e4796518f92f04c50d8e5cbe6bdc70d4c49f
kdnakt <a.kid.1985@gmail.com>
2023-12-17 11:09:45 +0900

    initialize GDT

--- src/gdt.rs
+++ src/gdt.rs
@@ -1,5 +1,11 @@
 use x86_64::VirtAddr;
-use x86_64::structures::tss::TaskStateSegment;
+use x86_64::structures::{
+    tss::TaskStateSegment,
+    gdt::{
+        GlobalDescriptorTable,
+        Descriptor,
+    },
+};
 use lazy_static::lazy_static;
 
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
@@ -18,4 +24,17 @@ lazy_static! {
         };
         tss
     };
+}
+
+lazy_static! {
+    static ref GDT: GlobalDescriptorTable = {
+        let mut gdt = GlobalDescriptorTable::new();
+        gdt.add_entry(Descriptor::kernel_code_segment());
+        gdt.add_entry(Descriptor::tss_segment(&TSS));
+        gdt
+    };
+}
+
+pub fn init() {
+    GDT.load();
 }
