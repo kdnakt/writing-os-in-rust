4ff2f5ce2e513cfc6665b08cb0371bdb108c652c
kdnakt <a.kid.1985@gmail.com>
2023-12-19 12:52:43 +0900

    notify end of interrupt

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -49,6 +49,11 @@ extern "x86-interrupt" fn timer_interrupt_handler(
     _stack_frame: InterruptStackFrame,
 ) {
     print!(".");
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
 }
 
 extern "x86-interrupt" fn breakpoint_handler(
