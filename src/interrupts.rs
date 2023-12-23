6aba1d9af9334c369b67cbadc107b23444bc6e77
kdnakt <a.kid.1985@gmail.com>
2023-12-23 11:13:19 +0900

    add page fault handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,7 +1,9 @@
 use x86_64::structures::idt::{
     InterruptDescriptorTable,
     InterruptStackFrame,
+    PageFaultErrorCode,
 };
+use crate::hlt_loop;
 use crate::println;
 use crate::print;
 use crate::gdt;
@@ -22,6 +24,7 @@ lazy_static! {
             .set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()]
             .set_handler_fn(keyboard_interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt
     };
 }
@@ -104,6 +107,19 @@ extern "x86-interrupt" fn double_fault_handler(
     stack_frame: InterruptStackFrame, _error_code: u64
 ) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
 
 pub const PIC_1_OFFSET: u8 = 32;
