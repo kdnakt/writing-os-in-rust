e1272a7299e704b04352b21715adfed2f545bc74
kdnakt <a.kid.1985@gmail.com>
2023-12-24 15:09:08 +0900

    level 4 page table

--- src/main.rs
+++ src/main.rs
@@ -73,12 +73,17 @@ pub extern "C" fn _start() -> ! {
 
     // cause page fault by accessing memory outside the kernel
     // let ptr = 0xdeadbeaf as *mut u8;
-    let ptr = 0x20514b as *mut u8;
-    unsafe { let x = *ptr; }
-    println!("read worked");
+    // let ptr = 0x20514b as *mut u8;
+    // unsafe { let x = *ptr; }
+    // println!("read worked");
 
-    unsafe { *ptr = 42; }
-    println!("write worked");
+    // unsafe { *ptr = 42; }
+    // println!("write worked");
+
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
 
     #[cfg(test)]
     test_main();
