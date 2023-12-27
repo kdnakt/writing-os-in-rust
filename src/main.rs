2a0a6163b51014c6afd2e11d1c2c69b95b17880a
kdnakt <a.kid.1985@gmail.com>
2023-12-27 09:40:50 +0900

    print l3 pagetable entry

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,7 @@ entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use blog_os::memory::active_level_4_table;
     use x86_64::VirtAddr;
+    use x86_64::structures::paging::PageTable;
 
     // let vga_buffer = 0xb8000 as *mut u8;
 
@@ -101,6 +102,17 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     for (i, entry) in l4_table.iter().enumerate() {
         if !entry.is_unused() {
             println!("L4 Entry {}: {:?}", i, entry);
+
+            let phys = entry.frame().unwrap().start_address();
+            let virt = phys.as_u64() + boot_info.physical_memory_offset;
+            let ptr = VirtAddr::new(virt).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!("  L3 Entry {}: {:?}", i, entry);
+                }
+            }
         }
     }
 
