4869c1aaf64e3aabbc2122e54ee9a58bc88d681c
kdnakt <a.kid.1985@gmail.com>
2024-01-08 08:46:34 +0900

    use LockedHeap allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -11,6 +11,7 @@ use x86_64::{
     },
     VirtAddr,
 };
+use linked_list_allocator::LockedHeap;
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
@@ -41,7 +42,8 @@ pub fn init_heap(
 }
 
 #[global_allocator]
-static ALLOCATOR: Dummy = Dummy;
+// static ALLOCATOR: Dummy = Dummy;
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 pub struct Dummy;
 
