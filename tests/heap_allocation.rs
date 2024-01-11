c6b7c95955706b58eba2f4d48ca9b3380a764f76
kdnakt <a.kid.1985@gmail.com>
2024-01-12 00:47:54 +0900

    large vector allocation test

--- tests/heap_allocation.rs
+++ tests/heap_allocation.rs
@@ -9,6 +9,7 @@ extern crate alloc;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use alloc::boxed::Box;
+use alloc::vec::Vec;
 
 entry_point!(main);
 
@@ -41,4 +42,14 @@ fn simple_allocation() {
     let heap_value_2 = Box::new(13);
     assert_eq!(*heap_value_1, 41);
     assert_eq!(*heap_value_2, 13);
+}
+
+#[test_case]
+fn large_vec() {
+    let n = 1000;
+    let mut vec = Vec::new();
+    for i in 0..n {
+        vec.push(i);
+    }
+    assert_eq!(vec.iter().sum::<u64>(), (n - 1) * n / 2);
 }
