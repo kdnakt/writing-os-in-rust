3523ff6a23f02dccb510255b8b6e697a93f2b9ee
kdnakt <a.kid.1985@gmail.com>
2024-01-13 08:45:29 +0900

    allocate many boxes test

--- tests/heap_allocation.rs
+++ tests/heap_allocation.rs
@@ -10,6 +10,7 @@ use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use alloc::boxed::Box;
 use alloc::vec::Vec;
+use blog_os::allocator::HEAP_SIZE;
 
 entry_point!(main);
 
@@ -52,4 +53,12 @@ fn large_vec() {
         vec.push(i);
     }
     assert_eq!(vec.iter().sum::<u64>(), (n - 1) * n / 2);
+}
+
+#[test_case]
+fn many_boxes() {
+    for i in 0..HEAP_SIZE {
+        let x = Box::new(i);
+        assert_eq!(*x, i);
+    }
 }
