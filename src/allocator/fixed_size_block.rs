f733bda455d74def0ebb5d628d1d70c1e7374094
kdnakt <a.kid.1985@gmail.com>
2024-01-26 00:22:26 +0900

    allocate with fallback_allocator

--- src/allocator/fixed_size_block.rs
+++ src/allocator/fixed_size_block.rs
@@ -1,4 +1,7 @@
 
+use alloc::alloc::Layout;
+use core::ptr;
+
 const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128, 256, 512, 1024, 2048];
 
 struct ListNode {
@@ -21,5 +24,12 @@ impl FixedSizeBlockAllocator {
 
     pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
         self.fallback_allocator.init(heap_start, heap_size);
+    }
+
+    fn fallback_alloc(&mut self, layout: Layout) -> *mut u8 {
+        match self.fallback_allocator.allocate_first_fit(layout) {
+            Ok(ptr) => ptr.as_ptr(),
+            Err(_) => ptr::null_mut(),
+        }
     }
 }
