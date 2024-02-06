fcc218fb4c5b0a156e1918bb0b449519a0e6ffdf
kdnakt <a.kid.1985@gmail.com>
2024-02-06 10:12:26 +0900

    start with dummy RawWaker

--- src/task/simple_executor.rs
+++ src/task/simple_executor.rs
@@ -1,5 +1,9 @@
 use super::Task;
 use alloc::collections::VecDeque;
+use core::task::{
+    Waker,
+    RawWaker,
+};
 
 pub struct SimpleExecutor {
     task_queue: VecDeque<Task>,
@@ -15,4 +19,12 @@ impl SimpleExecutor {
     pub fn spawn(&mut self, task: Task) {
         self.task_queue.push_back(task)
     }
+}
+
+fn dummy_raw_waker -> RawWaker {
+    todo!();
+}
+
+fn dummy_waker() -> Waker {
+    unsafe { Waker::from_raw(dummy_raw_waker()) }
 }
